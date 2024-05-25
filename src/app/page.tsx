8839d55f72fac83a52f5e83da123e859721f802e
Brwa Yaseen <brwayasin29@gmail.com>
2024-05-25 16:05:58 +0300

    new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from "@/components/ui/command";
 import { useEffect, useState } from "react";
 export default function Home() {
   const [input, setInput] = useState<string>("")
@@ -24,10 +25,35 @@ export default function Home() {
       <div className=" flex flex-col gap-6 items-center pt-32 duration-500
        animate-in animate fade-in-5 slide-in-from-bottom-3">
         <h1 className=" text-5xl tracking-tight font-bold">Lightening Speed Search</h1>
-      <input type="text"
-       value={input}
-       onChange={(e)=>{setInput(e.target.value)}}
-       className=" text-zinc-900" />
+        <p className=" text-zinc-600 text-lg max-w-prose text-center">
+          Very fast search functionality built with Hono, Next, and Cloudflare
+          <br /> Search for country name below and get results almost instantly
+          </p>
+
+          <div className=" max-w-md w-full">
+            <Command>
+              <CommandInput value={input} onValueChange={setInput} placeholder="Search Countries"
+              className="placeholder:text-zinc-500" />
+              <CommandList>
+                {searchResults?.results.length === 0 ? (
+                  <CommandEmpty>No results were found</CommandEmpty>
+                ): null}
+
+                {searchResults?.results ? (
+                  <CommandGroup heading="Results">
+                    {searchResults?.results.map((result)=>(
+                      <CommandItem key={result}
+                       value={result}
+                       onSelect={setInput}>
+                        {result}
+                       </CommandItem>
+                    ))}
+                  </CommandGroup>
+                ): null}
+              </CommandList>
+            </Command>
+
+          </div>
        </div>
     </main>
   );
