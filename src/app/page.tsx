e6b870f1d7ee6ce1fd8e3cdcaea0c95ce2c14cc4
Brwa Yaseen <brwayasin29@gmail.com>
2024-05-25 17:11:02 +0300

    new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   const [input, setInput] = useState<string>("")
   const [searchResults, setSearchResults] = useState<{
     results: string[]
-    duraion: number
+    duration: number
   }>()
 
   useEffect(()=>{
@@ -15,6 +15,8 @@ export default function Home() {
         {return setSearchResults(undefined)} 
 
       const res = await fetch(`/api/search?q=${input}`)
+      const data = (await res.json()) as {results: string[]; duration: number}
+      setSearchResults(data)
     }
     fetchResults()
   },[input])
@@ -24,7 +26,7 @@ export default function Home() {
     <main className=" h-screen w-screen grainy">
       <div className=" flex flex-col gap-6 items-center pt-32 duration-500
        animate-in animate fade-in-5 slide-in-from-bottom-3">
-        <h1 className=" text-5xl tracking-tight font-bold">Lightening Speed Search</h1>
+        <h1 className=" text-5xl tracking-tight font-bold"> ⚡ Lightening Speed Search</h1>
         <p className=" text-zinc-600 text-lg max-w-prose text-center">
           Very fast search functionality built with Hono, Next, and Cloudflare
           <br /> Search for country name below and get results almost instantly
@@ -50,6 +52,16 @@ export default function Home() {
                     ))}
                   </CommandGroup>
                 ): null}
+
+                {searchResults?.results ? (
+                  <>
+                  <div className=" h-px w-full bg-zinc-200"/>
+                  <p className='p-2 text-xs text-zinc-500'>
+                    Found {searchResults.results.length} results in{' '}
+                    {searchResults?.duration.toFixed(0)}ms
+                  </p>
+                  </>
+                ) : null}
               </CommandList>
             </Command>
 
